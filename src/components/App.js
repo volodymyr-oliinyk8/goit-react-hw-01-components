b7ac6313078609d71b8dbb1618e637ac2ff5bb2f
volodymyr-oliinyk8 <oliynyk.vo@gmail.com>
2021-07-08 22:17:15 +0300

    add components profile

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Profile from './Profile/Profile';
+import user from '../data/user.json';
+
+
+const App = () => {
+    return (
+        <>
+            <Profile name={user.name} tag={user.tag} location={user.location} avatar={user.avatar} stats={user.stats} />
+        </>
+    );
+};
+
+export default App;
